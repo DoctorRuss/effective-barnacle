52ba4fab40602c4dc36501afd662ead319072eb6
DoctorRuss <russ@misfortu.net>
2024-10-28 09:02:33 +0000

    Change JSON shape, print days separately

--- importAndAnalyse.py
+++ importAndAnalyse.py
@@ -2,46 +2,55 @@ import json
 from datetime import datetime
 import os
 
-
 # Dictionary to group fixtures by date
 date_format = "%d/%m/%y"
 
 # Directory where your JSON files are stored
 directory = '.'  # Replace with the actual directory path
 
-# List to hold all fixtures
-all_fixtures = []
+def isSaturdayLeague(filename):
+    return filename.startswith('BSYL') or filename.startswith('SVYL')
 
+def sortAndPrintFixtures(fixtures):    
+    # Sort the fixtures by date
+    sorted_fixtures = sorted(fixtures, key=lambda x: datetime.strptime(x['date'], date_format))
+    # Print the grouped fixtures by date
+    for fixture in sorted_fixtures:
+        if 'Postponed' not in fixture['notes'] and fixture['home_team'].startswith('Rockleaze'):
+            print(f"{fixture['fixtype']}, {fixture['date']}, {fixture['home_team'].removeprefix('Rockleaze Rangers ').removeprefix('Youth ')}, {fixture['away_team']}, Hillside, {fixture['competition']}")        
+    
 # Read each JSON file and aggregate the data
+
+# List to hold all fixtures
+saturday_fixtures = []
+sunday_fixtures = []
+
+
 for filename in os.listdir(directory):
-    if filename.endswith('.json') and not filename.startswith('aggregated'):
+    if filename.endswith('.json'):
         with open(os.path.join(directory, filename), 'r') as json_file:
-            age_group_fixtures = json.load(json_file)
-            all_fixtures.extend(age_group_fixtures)
+            if isSaturdayLeague(filename):
+                saturday_fixtures.extend(json.load(json_file))
+            else:
+                sunday_fixtures.extend(json.load(json_file))
 
-
+print('\nSaturday Spreadsheet format \n')
+sortAndPrintFixtures(saturday_fixtures)
+print('\nSunday Spreadsheet format \n')
+sortAndPrintFixtures(sunday_fixtures)
 
 # Optional: Save the aggregated fixtures to a new JSON file
 #with open('aggregated_fixtures.json', 'w') as json_file:
 #    json.dump(all_fixtures, json_file, indent=4)
 
-
-# Print the grouped fixtures by date
-# Sort the fixtures by date
-sorted_fixtures = sorted(all_fixtures, key=lambda x: datetime.strptime(x['date'], date_format))
-
 #for fixture in sorted_fixtures:
 #    print(f"Date: {fixture['date']}, Home Team: {fixture['home_team']}, Away Team: {fixture['away_team']}, Fixture Type: {fixture['fixtype']} , Time: {fixture['time']}")
     
-for fixture in sorted_fixtures:
-    if 'Postponed' in fixture['notes']:
-        continue
-    else:
-        if fixture['home_team'].startswith('U'):
-            print(f"Date: {fixture['date']}, Home Team: {fixture['home_team']}, Away Team: {fixture['away_team']}, Fixture Type: {fixture['fixtype']} , Time: {fixture['time']}")
+#for fixture in saturday_sorted_fixtures:
+#    if 'Postponed' in fixture['notes']:
+#        continue
+#    elif fixture['home_team'].startswith('Rockleaze'):
+#        print(f"Date: {fixture['date']}, Home Team: {fixture['home_team'].removeprefix('Rockleaze Rangers ').removeprefix('Youth ')}, Away Team: {fixture['away_team']}, Fixture Type: {fixture['fixtype']} , Time: {fixture['time']}")
 
-print('\n Spreadsheet format \n')
 
-for fixture in sorted_fixtures:
-    if fixture['home_team'].startswith('U') and 'Postponed' not in fixture['notes']:
-        print(f"{fixture['fixtype']}, {fixture['date']}, {fixture['home_team']}, {fixture['away_team']}, Hillside, {fixture['competition']}")        
+
